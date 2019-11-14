dccb70dd476ce7d02aa75d7096cf96837dac9e36
Damien Martinez <damien.martinez1@epsi.fr>
2019-11-14 11:31:22 +0100

    Ajoutez des fichiers projet.

---
+++ Eval.ORM/ContexteBDD.cs
@@ -0,0 +1,18 @@
+﻿using DocumentFormat.OpenXml.Math;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eval.ORM
+{
+    public class ContexteBDD : DbContext
+    {
+        public ContexteBDD() : base("Eval")
+        {
+
+        }
+    }
+}
